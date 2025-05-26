a517dac07cfd756888c4c1abff327c917de49ba9
tanulo <tanulo@barossgabor.local>
2025-05-26 10:10:31 +0200

    like/unlike funkció

--- js/newMeme.js
+++ js/newMeme.js
@@ -1,69 +1,81 @@
-const icon = document.getElementsByName("icon-home")[0];
+// globális változók
 const iconHome = document.querySelector('.icon-home');
+const iconUser = document.querySelector('.icon-user');
+const iconLogout = document.querySelector('.icon-logout');
+const fileUpload = document.querySelector('#fileUpload');
+let meme = null; // elkőkép betöltéséhez
+const newMeme = document.querySelector('.newMeme'); // az előképet ide rakom be
+const uploadButton = document.querySelector('.uploadButton'); // a feltöltés gomb
 
+//console.log(iconHome);
+
+// események
 iconHome.addEventListener('click', () => {
-window.location.href= "../html/home.html";
+    window.location.href = '../html/home.html';
 });
 
-const profile = document.querySelector('.user-icon');
-profile.addEventListener('click', () => {
-    window.location.href= "../html/profile.html";
+iconUser.addEventListener('click', () => {
+    window.location.href = '../html/profile.html';
 });
 
-    
-const kijelentkezes = document.querySelector('.icon-logout') ;
-kijelentkezes.addEventListener('click', logout);
+iconLogout.addEventListener('click', logout);
+
+fileUpload.addEventListener('change', selectPicture);
+
+uploadButton.addEventListener('click', uploadMeme);
+
+// függvények
 async function logout() {
-    const respons = await fetch('http://127.0.0.1:3000/api/auth/logout', {
-        method: `POST`,
-        credentials: `include`
+    const response = await fetch('http://127.0.0.1:3000/api/auth/logout', {
+        method: 'POST',
+        credentials: 'include'
     });
 
-    const data = await respons.json();
-    console.log(data)
-    if (respons.ok) {
-        window.location.href = '../index.html'
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+    
+    if (response.ok) {
+        window.location.href = '../index.html';
     } else {
-        alert ('Hiba a kijelentkezéskor!')
+        alert('Hiba a kijelentkezéskor!');
     }
 }
 
-const newMeme = document.querySelector('.newMeme');
-
-const fileUpload = document.getElementById('fileUpload') ;
-fileUpload.addEventListener('change', selectPicture)
-
 function selectPicture() {
     const file = fileUpload.files[0];
-    console.log(file)
+    console.log(file);
+    
     if (file) {
         meme = file;
         const reader = new FileReader();
         reader.onload = (event) => {
-            newMeme.style.backgroundImage = `url('${event.target.result}')`
+            newMeme.style.backgroundImage = `url('${event.target.result}')`;
         }
-        reader.readAsDataURL(file)
+        reader.readAsDataURL(file);
     }
 }
 
-const uploadButton = document.querySelector('.uploadButton')
-uploadButton.addEventListener('click', uploadMeme);
-async function uploadMeme(){
+async function uploadMeme() {
     if (meme) {
         const formData = new FormData;
-        formData.append('meme',meme)
-        console.log(formData)
+        formData.append('meme', meme);
+        console.log(formData);
+        
         try {
-            const respons = await fetch('htpp://127.0.0.1:3000/api/meme/uploadsMeme', {
+            const response = await fetch('http://127.0.0.1:3000/api/memes/uploadMeme', {
                 method: 'POST',
                 body: formData,
                 credentials: 'include'
-            })
+            });
+
+            console.log(response);
+            
         } catch (error) {
-            console.log(error)
-            alert('Nem várt hiba!')
+            console.log(error);
+            alert('Nem várt hiba!');
         }
-    }else {
+    } else {
         alert('Válassz ki egy képet!');
     }
 }
